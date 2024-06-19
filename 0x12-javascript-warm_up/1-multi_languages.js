97db28d41bd1c7b497160903e0802facc2ddbfef
PrettyboyNonso <obii6776@gmail.com>
2024-06-19 14:11:16 -0700

    new Update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun \nPython is cool \nJavaScript is amazing');
+const statement1 = 'C is fun';
+const statement2 = 'Python is cool';
+const statement3 = 'Javascript is amazing';
+console.log(statement1);
+console.log(statement2);
+console.log(statement3);
