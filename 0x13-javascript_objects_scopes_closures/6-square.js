a38af6ea590ffb37b576e46ecfde01fe2f91a46d
PrettyboyNonso <135073822+PrettyboyNonso@users.noreply.github.com>
2024-06-20 23:43:03 +0100

    Update 6-square.js2

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 const inheritSquare = require('./5-square');
 class Square extends inheritSquare {
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.size; i++) {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
       let temp = '';
       for (let j = 0; j < this.size; j++) {
         temp = temp + c;
       }
       console.log(temp)
     }
+    } else {
+      super.print();
+    }
+    
   }
 }
 
