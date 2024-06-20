7094865bb87ebd0cf70e50428b2684bb4026b28c
PrettyboyNonso <obii6776@gmail.com>
2024-06-20 10:59:16 -0700

    fixed loop

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,10 +8,13 @@ class Rectangle {
   }
 
   print () {
+    let counts = '';
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        console.log('X');
+        counts = counts + 'X';
       }
+      console.log(counts);
+      counts = '';
     }
   }
 }
