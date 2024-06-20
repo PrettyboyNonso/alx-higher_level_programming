1ed2609c4d932b04a1a9180bb39c79182d9176fa
PrettyboyNonso <135073822+PrettyboyNonso@users.noreply.github.com>
2024-06-20 22:22:29 +0100

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,9 +2,7 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
  constructor (size) {
-   super(w, h);
-   this.width = this.size;
-   this.height = this.size;
+   super(size, size);
  }
 }
 
