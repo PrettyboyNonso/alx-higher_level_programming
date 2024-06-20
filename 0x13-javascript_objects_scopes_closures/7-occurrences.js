c65533c978ebf7c377b5ecd3b9a8342b0ded00ba
PrettyboyNonso <135073822+PrettyboyNonso@users.noreply.github.com>
2024-06-20 23:59:25 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement){
   let occurence = 0;
   for (let index = 0; index < list.length; index++) {
    if (list[index] === searchElement) {
-     occurence=+ 1;
+     occurence+= 1;
    }
     continue;
   }
