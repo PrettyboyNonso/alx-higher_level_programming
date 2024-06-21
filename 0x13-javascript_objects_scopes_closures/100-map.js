2a0b41b7165f6a8172ca5a0ed093e0170f0f3503
PrettyboyNonso <135073822+PrettyboyNonso@users.noreply.github.com>
2024-06-21 06:19:45 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that imports an array and computes a new array
 
-const array = require('./100-data');
+const array = require('./100-data').list;
 
 console.log(array);
 let cont = 0;
