30cbf9a794076f757fcf3392cb33ea356bb22598
PrettyboyNonso <obii6776@gmail.com>
2024-06-20 22:06:45 -0700

    General push

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,16 +4,15 @@ class Square extends inheritSquare {
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
-      let temp = '';
-      for (let j = 0; j < this.height; j++) {
-        temp = temp + c;
+        let temp = '';
+        for (let j = 0; j < this.height; j++) {
+          temp = temp + c;
+        }
+        console.log(temp);
       }
-      console.log(temp)
-    }
     } else {
       super.print();
     }
-    
   }
 }
 
