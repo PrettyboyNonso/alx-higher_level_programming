81ec91aa79e4b38e2dee36b46c576a881f8516d9
PrettyboyNonso <135073822+PrettyboyNonso@users.noreply.github.com>
2024-07-06 04:07:52 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+Script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response.
 """
 import requests
+import sys
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+
+    response = requests.post(url, data=data)
+    print(response.text)
