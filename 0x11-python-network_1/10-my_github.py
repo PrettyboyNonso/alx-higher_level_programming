8d3649250496a69e065680656786064fe027f8c3
PrettyboyNonso <obii6776@gmail.com>
2024-07-05 20:04:49 -0700

    I think i fixed it

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
 """
 import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users'
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print("None")
